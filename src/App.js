898722d8b8f888e0194cd90539172311990566df
Konrad <57108310+KonradGmur@users.noreply.github.com>
2020-07-28 18:14:50 +0200

    Add utils.js and ComponentA.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import "./styles.css";
 
+const ComponentA = React.lazy(() => import(".ComponentA"));
+
 export default function App() {
+  const [showComponent, setShowComponent] = useState();
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={() => setShowComponent(true)}> Pokaż komponent</button>
+      <Suspense fallback={<div>Ładowanie...</div>}>
+        {showComponent && <ComponentA />}
+      </Suspense>
     </div>
   );
 }
---
+++ src/ComponentA.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { add } from "./utils";
+
+const ComponentA = () => {
+  return <div>{add(1, 1)}</div>;
+};
+
+export default ComponentA;
